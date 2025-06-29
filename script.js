8d2949f96154595f2dc9a88a30e52ff450cad78a
camfpli2 <37838007+camfpli2@users.noreply.github.com>
2025-06-29 18:23:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function setup(){
 }
 
 
+function getIndex(person){
+  var personIndex=.5;
+  for(var j=0;j<wholeSchool.length;j++){
+    if(wholeSchool[j]===person){
+      personIndex=j;
+    }
+  }
+  if(personIndex===.5){   //detected a new person
+    
+  }
+}
+
+
